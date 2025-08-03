3c57b6509294cce11c011343e7e613a90bac74c1
janricayp <janricayp@gmail.com>
2025-08-03 16:43:18 +0800

    Find The Runner-up Score Problem Solution

---
+++ Python/find-the-runner-up-score.py
@@ -0,0 +1,23 @@
+# Given the participants' score sheet for your University Sports Day,
+# you are required to find the runner-up score.
+# You are given  scores. Store them in a list and find the score of the runner-up.
+
+# Input Format
+
+# The first line contains n. The second line contains an array A[] of integers each separated by a space.
+
+
+# Output Format
+
+# Print the runner-up score.
+
+if __name__ == '__main__':
+    n = int(input())
+    arr = map(int, input().split())
+
+    scores_without_duplicates = []
+    for i in arr:
+        if i not in scores_without_duplicates:
+            scores_without_duplicates.append(i)
+    scores_without_duplicates.sort()
+    print(scores_without_duplicates[-2])
