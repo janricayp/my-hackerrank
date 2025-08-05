807b7840cf6306ae7f9b39a24078483e20010bb4
janricayp <janricayp@gmail.com>
2025-08-06 01:46:49 +0800

    String Formatting Problem Solution

---
+++ Python/string-formatting.py
@@ -0,0 +1,21 @@
+# Given an integer, n, print the following values for each integer from 1 to n :
+
+# Decimal
+# Octal
+# Hexadecimal (capitalized)
+# Binary
+
+def print_formatted(number):
+    # your code goes here
+    width = len(bin(number)[2:]) 
+    for i in range(1, (number + 1)):
+        print(
+            str(i).rjust(width),
+            oct(i)[2:].rjust(width),
+            hex(i)[2:].upper().rjust(width),
+            bin(i)[2:].rjust(width)
+        )
+
+if __name__ == '__main__':
+    n = int(input())
+    print_formatted(n)
